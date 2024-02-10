b48b9886560f17ce1210f9b4443d6233c8f3d799
sara90-boubou <saraboubou4@gmail.com>
2024-02-10 15:42:03 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,12 +1,45 @@
-#!/usr/bin/python3
-""" Class user """
+#!/usr/bin/env python3
 
-from models.base_model import BaseModel
+"""
+Defines unittests for models/user.py.
+Unittest classes:
+    TestUserInstantiation
+    TestUserSave
+    TestUserToDict
+"""
+
+import unittest
+from models.user import User
 
 
-class User(BaseModel):
-    """ Class user """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class TestUserInstantiation(unittest.TestCase):
+    """Test instantiation of User class."""
+
+    def test_instance_creation(self):
+        user = User()
+        self.assertIsInstance(user, User)
+
+
+class TestUserSave(unittest.TestCase):
+    """Test saving User instances."""
+
+    def test_save_method(self):
+        user = User()
+        user.save()
+        self.assertIsNotNone(user.updated_at)
+
+
+class TestUserToDict(unittest.TestCase):
+    """Test converting User instance to dictionary."""
+
+    def test_to_dict_method(self):
+        user = User()
+        user_dict = user.to_dict()
+        self.assertIsInstance(user_dict, dict)
+        self.assertIn('id', user_dict)
+        self.assertIn('created_at', user_dict)
+        self.assertIn('updated_at', user_dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
