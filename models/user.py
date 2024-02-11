ae406169c6f8d5a0c517747bb5964249b4672665
sara90-boubou <saraboubou4@gmail.com>
2024-02-11 08:13:49 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -11,9 +11,3 @@ class User:
     def display_user_info(self):
         print(f"Username: {self.username}")
         print(f"Email: {self.email}")
-
-
-# Example usage:
-if __name__ == "__main__":
-    user1 = User("JohnDoe", "john.doe@example.com")
-    user1.display_user_info()
